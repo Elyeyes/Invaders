b6802540c7cdaaa8fe4403033eed329d64954de7
AlbabaTroSs <simon.lluch143@gmail.com>
2023-06-07 14:35:32 +0200

    game over / deplacement UFO /  affichage obstacle

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 import pygame as pg
 
 pg.init()  # initialise pygame objects
-screen = pg.display.set_mode((1920 / 1.25, 1080 / 1.25))  # créer une fenetre d'une taille (x,y), #on divise par 1.25 car la mise à l'echelle et de 125%
-pg.display.set_caption('Space Invaders')  # change le nom de la fenetre créée
 
+screen = pg.display.set_mode((1920 / 1.25,
+                              1080 / 1.25))  # créer une fenetre d'une taille (x,y), #on divise par 1.25 car la mise à l'echelle et de 125%
+pg.display.set_caption('Space Invaders')  # change le nom de la fenetre créée
+clock = pg.time.Clock()
 pixelfont = "Pixel Coleco.otf"
 velocity_ennemie = 5
 velocity_joueur = 1
@@ -23,11 +25,6 @@ class ColorsRGB:
     White = (255, 255, 255)
 
 
-"""class Forme:
-    def __init__(self,squid:Entity,)
-    if self.squid.etat=="""
-
-
 class Forme:
     squid1 = pg.image.load('squid1.png')
     crab1 = pg.image.load('crab1.png')
@@ -37,16 +34,28 @@ class Forme:
     octopus2 = pg.image.load('octopus2.png')
     UFO = pg.image.load('UFO.png')
     Laser = pg.image.load('Laser.png')
+    Projectile = pg.image.load('projectile.png')
 
-class Projectiles(pg.sprite.Sprite):
-    def __init__(self, x, y):
+
+class Projectile(pg.sprite.Sprite):
+    def __init__(self, position, image):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.image.load('projectile.png')
+        self.image = image
+        self.position = position
         self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
+        self.rect.center = [self.position[0], self.position[1]]
+        pg.draw.rect(screen, "white", ((500, 800), (500, 812)))
 
     def update(self):
         self.rect.y = -5
+
+
+class testproj:
+    def __init__(self, position):
+        self.position = position
+        while self.position[1] > 0:
+            pg.draw.rect(screen, "white",
+                         ((self.position[0], self.position[0] + 4), (self.position[1], self.position[1] + 16)))
 
 
 class Entity:
@@ -56,12 +65,12 @@ class Entity:
         self.life = life
         self.position = position
         self.velocity = velocity
+        self.rect = self.image.get_rect()
 
 
 # sous-classe joueur
 class joueur(Entity):
     score_joueur = 0
-
     # fonction déplacement du joueur
     def deplacement(self):
         if pg.key.get_pressed():
@@ -118,13 +127,11 @@ class spaceinvaders:
     def __init__(self):
         pg.init()"""
 
-UFO = UFO("UFO", Forme.UFO, 1, (50, 10), velocity_ennemie)
-Laser = joueur("Laser", Forme.Laser, 3, (744, 800), velocity_joueur)
-
 
 # génération automatique des rangées d'ennemies
 def autogeneration():
-    octopus0 = Ennemie("octopus0", Forme.octopus1, 1, (430, 180), velocity_ennemie)  # initialise une entité servant de référence pour la position de toutes les autres
+    octopus0 = Ennemie("octopus0", Forme.octopus1, 1, (430, 180),
+                       velocity_ennemie)  # initialise une entité servant de référence pour la position de toutes les autres
     for i in range(0, 5):
         for j in range(1, 12):
             if i == 0:
@@ -148,8 +155,17 @@ def autogeneration():
                 png = Forme.octopus1
                 mul = 0
             autoennemie = nomennemie + str(j)  # nom nouvelle ennemie
-            globals()[autoennemie] = Ennemie(autoennemie, png, 1, (octopus0.position[0] + j * 58 + mul, octopus0.position[1] + i * 60), velocity_ennemie)
-    # Affichage auto des ennemies précédement créée
+            globals()[autoennemie] = Ennemie(autoennemie, png, 1,
+                                             (octopus0.position[0] + j * 58 + mul, octopus0.position[1] + i * 60),
+                                             velocity_ennemie)
+
+
+UFO = UFO("UFO", Forme.UFO, 1, (50, 10), 8)
+Laser = joueur("Laser", Forme.Laser, 3, (730, 800), velocity_joueur)
+Tir = Projectile((500, 800), Forme.Projectile)
+
+
+def autoaffichage():  # Affichage auto des ennemies précédement créée
     for i in range(0, 5):
         for j in range(1, 12):
             if i == 0:
@@ -165,6 +181,16 @@ def autogeneration():
             inst_name = nomennemie + str(j)
             screen.blit(globals()[inst_name].image, globals()[inst_name].position)
 
+def game_over_screen():
+   screen.fill((0, 0, 0))
+   font = pg.font.SysFont('arial', 40)
+   title = font.render('Game Over', True, (255, 255, 255))
+   restart_button = font.render('R - Restart', True, (255, 255, 255))
+   quit_button = font.render('Q - Quit', True, (255, 255, 255))
+   screen.blit(title, (1080/3 - title.get_width()/2, 1980/3 - title.get_height()/3))
+   screen.blit(restart_button, (1080/3 - restart_button.get_width()/2, 1980/3 + restart_button.get_height()))
+   screen.blit(quit_button, (1080/3 - quit_button.get_width()/2, 1980/3 + quit_button.get_height()/2))
+   pg.display.update()
 
 # fonction scoreboard
 def scoreboard():
@@ -187,29 +213,52 @@ def scoreboard():
         score=score+100"""
 
 
+autogeneration()
+RectLaser2 = pg.image.load('Laser.png').get_rect()
+RectLaser2.topleft = (400, 825)
+
+
 def affichage():
     screen.fill("black")
-    autogeneration()
+    autoaffichage()
+    for i in range(0, 4):
+        nom = "polygon" + str(i)
+        nom = (
+        (370 + i * 225, 570), (390 + i * 225, 550), (470 + i * 225, 550), (490 + i * 225, 570), (490 + i * 225, 670),
+        (470 + i * 225, 670), (450 + i * 225, 650), (410 + i * 225, 650), (390 + i * 225, 670), (370 + i * 225, 670))
+        pg.draw.polygon(screen, "green", nom)
+
     screen.blit(UFO.image, UFO.position)  # affichage de l'UFO
-    screen.blit(Laser.image, Laser.position)  # affichage du joueur
+    screen.blit(Tir.image, Tir.position)  # affichage du joueur
+    UFO.deplacement()
+    Laser.rect.topleft = (Laser.position)
+    screen.blit(Laser.image, Laser.rect)
+    screen.blit(pg.image.load('Laser.png'), RectLaser2)
+    collide = pg.Rect.colliderect(Laser.rect, RectLaser2)
+    if collide:
+        RectLaser2.topleft = (400, 420)
+
     # créer les bordures
     screen.fill("black", (0, 0, 400 / 1.25, 1080 / 1.25))
     screen.fill("black", (1520 / 1.25, 0, 400 / 1.25, 1080 / 1.25))
     scoreboard()  # appel de la fonction scoreboard
     if Laser.life <= 0:
-        screen.fill(ColorsRGB.Purple)  # game over temporaire
+        game_over_screen()  # game over temporaire
+
+
+def quitter():
+    if event.type == pg.MOUSEBUTTONDOWN:
+        print(pg.mouse.get_pos())
+        Laser.life = Laser.life - 1
+    if event.type == pg.QUIT:
+        pg.quit()
+        quit()
 
 
 while True:
     affichage()
     pg.display.update()
-    pg.time.Clock().tick(500)
-    pg.key.set_repeat(1)  # Permet au touche du clavier de continuer de fonctionner une fois enfoncé
     for event in pg.event.get():
         Laser.deplacement()  # appel fonction déplacement dans class joueur (pour le Laser)
-        if event.type == pg.MOUSEBUTTONDOWN:
-            print(pg.mouse.get_pos())
-            Laser.life = Laser.life - 1
-        if event.type == pg.QUIT:
-            pg.quit()
-            quit()
+        quitter()
+        pg.key.set_repeat(1)  # Permet au touche du clavier de continuer de fonctionner une fois enfoncé
